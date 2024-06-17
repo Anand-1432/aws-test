0d23cba304749b7018b5261920e18a4a53a996d2
Anand-Techdome <anand.gaikwad@techdome.com>
2024-06-17 23:15:56 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+const data = [{
+    name: "anand",
+    batch: 2024,
+    age: 22
+}]
+
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: data
+    })
+})
+
+app.listen(port, () => {
+    console.log(`listening at port ${port}`);
+})
